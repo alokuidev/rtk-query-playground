b3229333a5d3b76dc9ead45460858b9cfc115a40
warrioralok <34855411+warrioralok@users.noreply.github.com>
2025-04-02 21:01:46 +0530

    Query Client implemented

--- src/Component/About.jsx
+++ src/Component/About.jsx
@@ -1,27 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import React  from 'react'
 import { fetchApi } from '../Api/axios';
 
 const About = () => {
-  const [posts, setPosts] = useState([]);
-  
-  const getPostData = async () =>{
-    try {
-        const res = await fetchApi();
-        res.status === 200 ? setPosts(res.data) : [];
-        console.log(res)
-    } catch (error) {
-        console.log(error);
-        return [];
-    }
-  }
 
-  useEffect(() =>{
-    getPostData();
-  },[])
+  const getPostData = async () =>{
+      try {
+          const res = await fetchApi();
+          //console.log(res.data)
+          return res.status === 200 ? res.data : [];
+          //console.log(res)
+      } catch (error) {
+          console.log(error);
+          return [];
+      }
+    }
+
+  const {data} = useQuery({
+      queryKey:['posts'],
+      queryFn:getPostData
+    })
   return (
     <main className="main-content">
       <h2>About Component (Full Width)</h2>
-      {posts?.map((elem) =>{
+      {data?.map((elem) =>{
           const {id, title, body} = elem;
           return(
             <p key={id} style={{borderLeft:'5px solid #000',textAlign:'left',padding:'20px',margin:'0 auto',marginTop:'50px', backgroundColor:'#c6c6c6', maxWidth:'70%'}}>
